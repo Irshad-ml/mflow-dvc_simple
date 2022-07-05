b4535971f38bba6c2c7a1ba1f528b866d4c28a7b
Md Irshad <m3irshad3@gmail.com>
2022-07-05 14:44:01 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="Irshad", 
+    packages=find_packages(),
+    license="MIT"
+)
---
+++ tests/test_config.py
@@ -0,0 +1,4 @@
+def test_generic():
+    a=10
+    b=10
+    assert a==b
