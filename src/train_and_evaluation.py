8d1eb69040e59bbdab6eb9eabeadabd2053d7cb9
Md Irshad <m3irshad3@gmail.com>
2022-07-05 11:59:54 +0530

    Stage 3 completed and also tracker added

--- src/train_and_evaluation.py
+++ src/train_and_evaluation.py
@@ -60,6 +60,27 @@ def train_and_evaluate(config_path):
     print("  R2: %s" % r2)
     
     
+    #####################################################
+    metrics_file = config["reports"]["metrics"]
+    params_file = config["reports"]["params"]
+
+    with open(metrics_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+        }
+        json.dump(params, f, indent=4)
+#####################################################
+    
+    
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.joblib")
 
